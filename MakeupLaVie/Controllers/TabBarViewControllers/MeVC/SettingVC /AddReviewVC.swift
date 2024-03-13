8a68a2545ccd1fa27747efb4691e5dc92d93d208
starsItDev <144766845+starsItDev@users.noreply.github.com>
2024-03-13 11:16:19 +0500

    13 March 2024 - AddReviewApi

--- MakeupLaVie/Controllers/TabBarViewControllers/MeVC/SettingVC /AddReviewVC.swift
+++ MakeupLaVie/Controllers/TabBarViewControllers/MeVC/SettingVC /AddReviewVC.swift
@@ -6,24 +6,45 @@
 //
 
 import UIKit
+import Cosmos
 
 class AddReviewVC: UIViewController {
 
+    
+    @IBOutlet weak var cosmosRating: CosmosView!
+    @IBOutlet weak var reviewTxtView: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func backButtonButton(_ sender: UIButton) {
+        self.navigationController?.popViewController(animated: false)
     }
-    */
 
+   @IBAction func submitButton(_ sender: UIButton) {
+      let params: [String: Any] = [
+        "rating": "\(cosmosRating.rating)",
+         "review": reviewTxtView.text ?? "",
+         "recommend": "1"
+     ]
+     postCompleteOrderAPICall(params: params)
+ }
+    func postCompleteOrderAPICall(params: [String: Any]) {
+        let url = base_url + "product/reviews/11/create"
+        Networking.instance.postApiCall(url: url, param: params) { (response, error, statusCode) in
+            if error == nil {
+                if statusCode == 200 {
+                    if let vc = UIStoryboard.init(name: "Main", bundle: Bundle.main).instantiateViewController(withIdentifier: "MyReviewsVC") as? MyReviewsVC {
+                        self.navigationController?.pushViewController(vc, animated: true)
+                    }
+                }
+                else {
+                    print("Status code: \(statusCode)")
+                }
+            } else {
+                print("API call error")
+            }
+        }
+    }
 }
